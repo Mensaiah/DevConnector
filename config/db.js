b900d5610ab2d222fe349b4975334e81966975b8
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-07-12 17:12:26 +0100

    create route files

--- config/db.js
+++ config/db.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose"),
 const connectDB = async () => {
   try {
     await mongoose.connect(db, { useNewUrlParser: true });
+    ``;
+    console.log("MongoDB connected...");
   } catch (err) {
     console.log(err.mmessage);
   }
