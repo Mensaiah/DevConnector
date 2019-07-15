ae79e847bd59c3f58d8164bd0ac872e90454b790
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-07-15 17:01:13 +0100

    current user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
 
-// @route GET api/profile
+// @route GET api/profile/me
 // @desc  Test route
 // @access private
-router.get("/", (req, res) => {
-  res.send("Profile route");
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile available" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
