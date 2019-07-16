b413b1544be3f96fc52909d90d33fd6d7f96aa9f
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-07-16 12:46:09 +0100

    get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -103,5 +103,37 @@ router.post(
     }
   }
 );
+// @route GET api/profile
+// @desc  get all Profiles
+// @access public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+// @route GET api/profile/user/:user_id
+// @desc  get profile by user id
+// @access public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "Objectid") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
