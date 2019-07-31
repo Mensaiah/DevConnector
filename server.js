9707691b889bb56b810ed93524f4542fd37dfa22
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-07-31 18:41:42 +0100

    Ready for Production

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "dist", "html", "index.html")
+    );
   });
 }
 
