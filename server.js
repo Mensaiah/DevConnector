c09efeec9ce808f15464709a9126201d50567a1c
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-07-31 18:37:24 +0100

    ready for production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 //Connect Database
 connectDB();
 
@@ -9,14 +10,18 @@ app.use(cors());
 //Initialize Body Parser
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("App working");
-});
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
