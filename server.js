fff5de4d87e5374aab978589318033798612026c
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-08-01 16:44:51 +0100

    set up for production

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "dist", "html", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
