5cf5db9b016b4baeb5220725e39e3e920956c45a
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-08-01 18:24:28 +0100

    set up for production

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+app.use("/", express.static("client/dist"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/dist"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "dist", "html", "index.html")
-    );
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "client", "dist", "html", "index.html")
+//     );
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
